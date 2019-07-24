63e69d7cd7906e2400e06579a9c153625ded212d
sheetal-laad <52482149+sheetal-laad@users.noreply.github.com>
2019-07-24 13:00:10 +0530

    Add files via upload

---
+++ SpringProject/SpringDemo/src/main/java/springcore_example/Main.java
@@ -0,0 +1,18 @@
+package springcore_example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	private static ApplicationContext context;
+
+	public static void main(String[] args) {
+		context=new ClassPathXmlApplicationContext("beans.xml");
+		HelloBean hellobean=(HelloBean)context.getBean("HelloBean");
+		hellobean.sayHello();
+		
+		
+
+	}
+
+}
